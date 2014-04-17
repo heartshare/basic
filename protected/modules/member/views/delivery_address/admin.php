99d57a757d8cd91cc42040b5c4a8e877fbf9ca3a
changhai.lin <1079140464@qq.com>
2014-04-17 14:50:52 +0800

    delivery_address

--- protected/modules/member/views/delivery_address/admin.php
+++ protected/modules/member/views/delivery_address/admin.php
@@ -46,31 +46,41 @@ $this->breadcrumbs = array(
             <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
         </div>
          <span id="item" style="margin:30px 0px 20px 0px;">已保存有效的地址:</span>
-        <table class="table address-table"  >
-            <thead>
-            <tr>
-                <th class="col-xs-1">收件人</th>
-                <th class="col-xs-2">所在地区</th>
-                <th class="col-xs-2">街道地址</th>
-                <th class="col-xs-1">邮政编码</th>
-                <th class="col-xs-2">手机</th>
-                <th class="col-xs-2">&nbsp;</th>
-                <th class="col-xs-2">操作</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <td>小明</td>
-                <td>河北省秦皇岛市北戴河区</td>
-                <td>11111</td>
-                <td>1111111</td>
-                <td>13338888899</td>
-                <td><a href="">设为默认地址</a></td>
-                <td><a href="">修改</a>|<a href="">删除</a></td>
-            </tr>
-            </tbody>
 
-        </table>
+            <?php $this->widget('zii.widgets.grid.CGridView', array(
+                'dataProvider'=>$dataProvider,
+                'columns'=>array(
+                    'contact_name',
+                    's.name',
+                    'c.name',
+                    'd.name',
+                    'address',
+                    'zipcode',
+                    'phone' ,
+                    'mobile_phone' ,
+                    'memo' ,
+                    array(
+                        'name' => 'is_default',
+                        'value' => 'Tbfunction::ShowYesOrNo($data->is_default)',
+                    ),
+                    array(
+                        'name' => 'create_time',
+                        'value' => 'date("Y-m-d", $data->create_time)',
+                        'htmlOptions' => array('style'=>'width:100px')
+                    ),
+                    array(
+                        'name' => 'update_time',
+                        'value' => 'date("Y-m-d", $data->update_time)',
+                        'htmlOptions' => array('style'=>'width:100px')
+                    ),
+                    array(
+                        'class' => 'CButtonColumn',
+                        'template' => '{view}{update}{delete}',
+                        'viewButtonUrl' => 'Yii::app()->createUrl("/member/delivery_address/view/id/".$data->contact_id)',
+                    ),
+                ),
+            )); ?>
+
 
     </div>
 
--- protected/modules/member/views/delivery_address/update.php
+++ protected/modules/member/views/delivery_address/update.php
@@ -15,7 +15,7 @@ $this->menu=array(
 
 <div class="box">
     <div class="box-title">更新收货地址#<?php echo $model->contact_id; ?></div>
-    <div class="box-content">
+    <div class="form-horizontal">
 <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
     </div>
 </div>
