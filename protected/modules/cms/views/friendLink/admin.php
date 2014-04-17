dea7c9b016984a3555744c3087ff55a3df489ff8
houweibo <wei.hou@jago-ag.cn>
2014-04-17 13:27:25 +0800

    换成中文的

--- protected/modules/cms/views/friendLink/admin.php
+++ protected/modules/cms/views/friendLink/admin.php
@@ -9,8 +9,8 @@ $this->breadcrumbs=array(
 );
 
 $this->menu=array(
-	array('label'=>'友情衔接列表', 'icon'=>'list', 'url'=>array('index')),
-	array('label'=>'创建友情衔接', 'icon'=>'plus', 'url'=>array('create')),
+	array('label'=>Yii::t('backend','友情衔接列表'), 'icon'=>'list', 'url'=>array('index')),
+	array('label'=> Yii::t('backend','创建友情衔接'), 'icon'=>'plus', 'url'=>array('create')),
 );
 
 Yii::app()->clientScript->registerScript('search', "
@@ -27,7 +27,8 @@ $('.search-form form').submit(function(){
 ");
 ?>
 
-<h1>管理友情衔接</h1>
+    <h1><?php echo Yii::t('backend','管理友情链接');?></h1>
+
 
 <p>
     You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>
